5bdb9c34e59237e13bd5bc28730e065f9a6e7cf2
SHAIKANASBASHA-55 <2300032367@kluniversity.in>
2025-10-07 12:24:58 +0530

    first commit

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/auth";
+const API_URL = "http://localhost:8081/auth"; // ✅ no /api prefix
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
